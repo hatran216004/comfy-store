a8d8655f3db791b4b98b8d022af4dc2af772d819
hatran216004 <qiqisokiu.321@gmail.com>
2025-01-02 08:49:13 +0700

    create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,65 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {
+  About,
+  Cart,
+  Checkout,
+  Error,
+  HomeLayout,
+  Landing,
+  Login,
+  Orders,
+  Products,
+  Register,
+  SingleProduct
+} from './pages';
+
 function App() {
-  return (
-    <>
-      <h1 className="text-3xl font-bold text-red-400">Hello world!</h1>
-      <button className="btn btn-neutral">Neutral</button>
-    </>
-  );
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <HomeLayout />,
+      errorElement: <Error />,
+      children: [
+        {
+          index: true,
+          element: <Landing />
+        },
+        {
+          path: 'products',
+          element: <Products />
+        },
+        {
+          path: 'products/:id',
+          element: <SingleProduct />
+        },
+        {
+          path: 'cart',
+          element: <Cart />
+        },
+        { path: 'about', element: <About /> },
+        {
+          path: 'checkout',
+          element: <Checkout />
+        },
+        {
+          path: 'orders',
+          element: <Orders />
+        }
+      ]
+    },
+    {
+      path: '/login',
+      element: <Login />,
+      errorElement: <Error />
+    },
+    {
+      path: '/register',
+      element: <Register />,
+      errorElement: <Error />
+    }
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
