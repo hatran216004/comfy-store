279d6aeae193270616f5bbcc1a321b7ff5bb2f47
hatran216004 <qiqisokiu.321@gmail.com>
2025-01-02 08:10:04 +0700

    initial project

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-red-400">Hello world!</h1>
+      <button className="btn btn-neutral">Neutral</button>
+    </>
+  );
+}
+
+export default App;
